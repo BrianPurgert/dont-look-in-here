8e88229281d15260f738ab13ce3b78b29ce9fabd
Brian Purgert <brianpurgert2@gmail.com>
2023-04-28 00:44:44 -0400

    matz

---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app :theme="color">
+    <v-app-bar>
+
+        <template v-slot:prepend>
+            <img src="/bp.svg" height="38" class="mr-2" alt="BP">
+        </template>
+
+
+      <v-spacer></v-spacer>
+      <v-btn
+        :prepend-icon="color === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        @click="onClick"
+        ></v-btn
+      >
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+          <nuxt-page />
+      </v-container>
+
+        <v-footer color="transparent" class="pt-5 pb-5">
+
+        </v-footer>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const color = ref('dark');
+
+function onClick() {
+  color.value = color.value === 'light' ? 'dark' : 'light';
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+const theme = useTheme();
+const app = useNuxtApp();
+
+</script>
+
+
+
+<template>
+ 
+      
+            <v-container>
+
+            </v-container>
+            <v-divider class="mt-2"></v-divider>
+      
+</template>
+
+<script>
+
+
+export default {
+	components: {
+		
+	},
+	data() {
+		return {
+
+		}
+	}
+}
+</script>
